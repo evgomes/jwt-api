9790b2e8209b32cae6e930c5eb164b71dc4da57f
Rebin <rebinm499@gmail.com>
2024-01-10 18:33:18 +0330

    Refactor :  - method's and variable's names changed  - Method extracted from ValidatePassword()

--- src/JWTAPI/JWTAPI/Security/Hashing/PasswordHasher.cs
+++ src/JWTAPI/JWTAPI/Security/Hashing/PasswordHasher.cs
@@ -7,70 +7,75 @@ namespace JWTAPI.Security.Hashing;
 /// </summary>
 public class PasswordHasher : IPasswordHasher
 {
-	public string HashPassword(string password)
-	{
-		byte[] salt;
-		byte[] buffer2;
-		if (string.IsNullOrEmpty(password))
-		{
-			throw new ArgumentNullException(nameof(password));
-		}
-		using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8, HashAlgorithmName.SHA512))
-		{
-			salt = bytes.Salt;
-			buffer2 = bytes.GetBytes(0x20);
-		}
-		byte[] dst = new byte[0x31];
-		Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
-		Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
-		return Convert.ToBase64String(dst);
-	}
+    public string HashPassword(string password)
+    {
+        byte[] salt;
+        byte[] passwordHashKey;
+        if (string.IsNullOrEmpty(password))
+        {
+            throw new ArgumentNullException(nameof(password));
+        }
+        using (Rfc2898DeriveBytes Key = new Rfc2898DeriveBytes(password, 0x10, 0x3e8, HashAlgorithmName.SHA512))
+        {
+            salt = Key.Salt;
+            passwordHashKey = Key.GetBytes(0x20);
+        }
+        byte[] storingPasswordArray = new byte[0x31];
+        Buffer.BlockCopy(salt, 0, storingPasswordArray, 1, 0x10);
+        Buffer.BlockCopy(passwordHashKey, 0, storingPasswordArray, 0x11, 0x20);
+        return Convert.ToBase64String(storingPasswordArray);
+    }
 
-	public bool PasswordMatches(string providedPassword, string passwordHash)
-	{
-		byte[] buffer4;
-		if (passwordHash == null)
-		{
-			return false;
-		}
-		if (providedPassword == null)
-		{
-			throw new ArgumentNullException(nameof(providedPassword));
-		}
-		byte[] src = Convert.FromBase64String(passwordHash);
-		if ((src.Length != 0x31) || (src[0] != 0))
-		{
-			return false;
-		}
-		byte[] dst = new byte[0x10];
-		Buffer.BlockCopy(src, 1, dst, 0, 0x10);
-		byte[] buffer3 = new byte[0x20];
-		Buffer.BlockCopy(src, 0x11, buffer3, 0, 0x20);
-		using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(providedPassword, dst, 0x3e8, HashAlgorithmName.SHA512))
-		{
-			buffer4 = bytes.GetBytes(0x20);
-		}
-		return ByteArraysEqual(buffer3, buffer4);
-	}
+    public bool ValidatePassword(string userInputData, string storedPasswordHash)
+    {
+        if (string.IsNullOrEmpty(storedPasswordHash))
+            return false;
+        if (userInputData is null)
+            throw new ArgumentNullException(userInputData, "User Password should not be null !");
 
-	[MethodImpl(MethodImplOptions.NoOptimization)]
-	private bool ByteArraysEqual(byte[] a, byte[] b)
-	{
-		if (ReferenceEquals(a, b))
-		{
-			return true;
-		}
+        byte[] decodedStoredPasswordHash = Convert.FromBase64String(storedPasswordHash);
 
-		if (a == null || b == null || a.Length != b.Length)
-		{
-			return false;
-		}
+        if (IsInvalid(decodedStoredPasswordHash))
+            return false;
 
-		bool areSame = true;
-		for (int i = 0; i < a.Length; i++)
-		{
-			areSame &= (a[i] == b[i]);
-		}
-		return areSame;
-	}
+        byte[] salt = ExtractPortion(decodedStoredPasswordHash, 1, 0x10);
+        byte[] expectedPassword = ExtractPortion(decodedStoredPasswordHash,0x11, 0x20);
+
+        byte[] GeneratedKey = DeriveKeyFromPassword(userInputData,salt);
+
+        return ByteArraysEqual(expectedPassword, GeneratedKey);
+       
+    }
+
+    private bool IsInvalid(byte[] hashed)
+    {
+        return hashed.Length != 0x31 || hashed[0] != 0;
+    }
+
+    private byte[] DeriveKeyFromPassword(string userInputData, byte[] salt)
+    {
+        using (Rfc2898DeriveBytes key = new(userInputData,salt, 0x3e8, HashAlgorithmName.SHA512))
+        {
+            return key.GetBytes(0x20);
+        }
+    }
+
+    private byte[] ExtractPortion(byte[] source, int offset, int length)
+    {
+        byte[] result = new byte[length];
+        Buffer.BlockCopy(source, offset, result, 0, length);
+        return result;
+    }
+
+    [MethodImpl(MethodImplOptions.NoOptimization)]
+    private bool ByteArraysEqual(byte[] a, byte[] b)
+    {
+        if (ReferenceEquals(a, b))
+            return true;
+
+        if (a == null || b == null || a.Length != b.Length)
+            return false;
+
+        return a.SequenceEqual(b);
+    }
 }
